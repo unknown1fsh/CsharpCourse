b684a4cc989b866677cd6337c447eddf462f5431
Selim Sercan ÇINAR <sercan.cinar@ordulu.com.tr>
2021-05-02 15:53:32 +0300

    Proje devam ediyor

---
+++ Conditionals/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Conditionals
+{ //ŞART BLOKLARIYLA ÇALIŞMAK
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var number = 11;
+            //if (number==10) //Eğer sayı 10 ise.  // == Eşittir
+            //{
+            //    Console.WriteLine("Number is 10");
+            //}
+            //else if(number ==20)
+            //{
+            //    Console.WriteLine("Number is 20");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Number is not 10 or 20");    
+            //}
+
+            //switch (number)
+            //{
+            //    case 10:
+            //        Console.WriteLine("Number is 10");
+            //        break;//burada olayı kes break
+            //    case 20:
+            //        Console.WriteLine("Number is 20");
+            //        break;
+            //    default://bunlardan hiçbiri değilse.
+            //        Console.WriteLine("Number is not 10 or 20");
+            //        break;
+            //}
+
+            //***CTRL + K 'dan sonra CTRL + D KODU OKUNAKLI HALE GETİRİR***
+
+            //if (number >= 0 && number <= 100)//&& ve demektir.
+            //{
+            //    Console.WriteLine("Number is betweed 0-100");
+            //}
+            //else if (number > 100 && number <= 200)
+            //{
+            //    Console.WriteLine("Number is betweed 101-200");
+            //}
+            //else if (number > 200 || number < 0) ; //   || işareti ya da veya demektir 2tane pipe dan oluşur. Sayımız 200'den büyükse veya sayımız küçüktür 0 sa if bloguna gir.
+            //{
+            //    Console.WriteLine("Number is less than 0 or greater than 200");
+            //}
+
+            //***İÇ İÇE İF BLOKLARIYLA ÇALIŞMAK***
+
+            if (number < 100)
+            {
+                if (number >= 90 && number < 95)
+                {
+                    ///
+                }
+                else
+                {
+                    ///dsadasd
+                }
+
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ TypesAndVariables/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TypesAndVariables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //CTRL + F5 Tuşu ile siyah ekran gelir ve bir tuşa basmamız istenir.(Console App)
+            //Console.WriteLine("Hello World!");
+            //Console.ReadLine(); (CTRL + F5 Tuşuna basmaya gerek kalmadan çalışan programı konsolda bekletir.
+
+            //** VALUE TYPES **//
+
+            // VERİ TİPLERİ
+
+            // byte a = 0 - 255 (1 byte)
+
+            // sbyte b = -128 / +127 (1 byte)
+
+            // short c = -32768 / +32767 (2 byte)
+
+            // ushort d = 0 - 65535 (2 byte)
+
+            // int e = -2 147 483 468 / + 2 147 483 648 (4 byte)
+
+            // uint f = 0 - 4 294 967 295 (4 byte)
+
+            // long g = -10²⁰ / +10²⁰ (8 byte)
+
+            // ulong h = 0 - 2 x 10²⁰ (8 byte)
+
+            // boolean - bool = Mantıksal veri tipidir tuttuğu değer true / false' dır.
+
+            // char character = 'A'  - Karakter veri tipi. Tek tırnak kullanılır.
+
+            // double i = 0.4; (Ondalıklı sayı tutar)
+
+            // decimal k = 0.18912612316; 
+
+            // var keyword'ü değişken tutmak için kullanılır.
+
+            // ENUM - Enum Sabitleri denir. Özetle şöyledir; Programda birçok değişkene tek tek sayısal değer vermek yerine "enum" kullanılabilir. Özet olarak "enum" yapısı sayıları anlamlı şekilde isimlendirerek kullanabilmeye izin verir.
+
+            // En genel "enum" bildirimi şu şekilde yapılır :
+
+            // public enum SehirPlakalari
+        // {
+        //     Ankara = 6, Kocaeli = 41, İzmir = 35, Adana = 1, Hatay = 31
+        // }
+
+
+        decimal number6 = 10.4m; // m harfi küçük ya da büyük olabilir M // 
+            double number5 = 10.4;
+            char character = 'A';
+            string city = "Ankara"; // city 6 karaterden oluşan bir dizidir.
+            bool condition = false;  //condition = şart
+            byte number4 = 0;
+            short number3 = 32767;
+            int number1 = 2147483647;
+            long number2 = 2147483648;
+            var number7 = 10;
+            number7 = 'A';
+            Console.WriteLine("Number1 is {0}", number1);
+            Console.WriteLine("Number2 is {0}", number2);
+            Console.WriteLine("Number3 is {0}", number3);
+            Console.WriteLine("Number4 is {0}", number4);
+            Console.WriteLine("Number5 is {0}", number5);
+            Console.WriteLine("Number6 is {0}", number6);
+            Console.WriteLine("Number7 is {0}", number7); //İlk atamamız integer olduğu için A karakteri dikkate alınmaz.
+            Console.WriteLine((int)Days.Friday); //Enum sabitleri değerlini 0'dan başlatır. Monday=10 Şeklinde de değer verilebilir 11,12 diye devam eder.
+            Console.WriteLine("Character is : {0}", (int)character); //Bunun çıktısı 65'dir 65 ASCII Karater dizesinde "A" harfine denk gelir.
+            Console.ReadLine();
+
+            
+        }
+
+    }
+    enum Days 
+    {
+        Monday=10,Tuesday,Wednesday,Thursday,Friday,Saturday,Sunday
+    }
+}
